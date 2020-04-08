ad444b21270b4bb5d8de890752c921cafde8975e
Bryan McBride <mcbride.bryan@gmail.com>
2020-04-08 17:39:54 -0400

    implement workbox precaching and update Holt Preserve map

--- service-worker.js
+++ service-worker.js
@@ -1,114 +1,56 @@
-/*jshint esversion: 6 */
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the 'License');
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an 'AS IS' BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+importScripts('assets/vendor/workbox-v5.1.2/workbox-sw.js');
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-12.15.19.2';
-const RUNTIME = 'runtime';
-
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  'manifest.json',
-  'maps.json',
-  'assets/img/apple-touch-icon.png',
-  'assets/img/android-chrome-192x192.png',
-  'assets/img/favicon-32x32.png',
-  'assets/img/favicon-16x16.png',
-  'assets/img/mhlc.png',
-  'assets/img/mhlc-logo.png',
-  'assets/img/crosshair.svg',
-  'assets/img/ios-share.png',
-  'assets/vendor/framework7-5.2.0/css/framework7.bundle.min.css',
-  'assets/vendor/openlayers-6.1.1/ol.css',
-  'assets/css/app.css',
-  'assets/fonts/MaterialIcons-Regular.woff2',
-  'assets/vendor/framework7-5.2.0/js/framework7.bundle.min.js',
-  'assets/vendor/openlayers-6.1.1/ol.js',
-  'assets/vendor/proj4js-2.5.0/proj4.js',
-  'assets/vendor/kompas-0.0.1/kompas.js',
-  'assets/js/app.js',
-  'maps/2018-Wolf-Creek-Falls-Preserve-Map.jpg',
-  'maps/AlbCo_RailTrail.jpg',
-  'maps/Bennett-Hill-Preserve-2017-new-logo.jpg',
-  'maps/Bozenkill_Preserve2.jpg',
-  'maps/Fox_Preserve-10-3-17.jpg',
-  'maps/Holt_Preserve.jpg',
-  'maps/MHLC-Keleher-Preserve-Update-2017.jpg',
-  'maps/Mosher-Marsh-Update-2017.jpg',
-  'maps/Normanskill_Preserve.jpg',
-  'maps/Phillipinkill_Preserve.jpg',
-  'maps/Schiffendecker_Preserve.jpg',
-  'maps/SchoharieCreek_Preserve.jpg',
-  'maps/StrawberryFields_Preserve.jpg',
-  'maps/Swift_Preserve1.jpg',
-  'maps/VanDyke_Entire_Preserve.jpg',
-  'maps/Winn_Preserve-2017-map.jpg'
-];
-
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
+workbox.setConfig({
+  debug: false,
+  modulePathPrefix: 'assets/vendor/workbox-v5.1.2/'
 });
 
-// The activate handler takes care of cleaning up old caches.
+workbox.precaching.precacheAndRoute([
+  {url: 'index.html', revision: '04.08.20.1'},
+  {url: './', revision: '04.08.20.1'}, // Alias for index.html
+  {url: 'index.html', revision: '04.08.20.1'},
+  {url: 'maps.json', revision: '04.08.20.2'},
+  {url: 'assets/img/apple-touch-icon.png', revision: '04.08.20.1'},
+  {url: 'assets/img/android-chrome-192x192.png', revision: '04.08.20.1'},
+  {url: 'assets/img/favicon-32x32.png', revision: '04.08.20.1'},
+  {url: 'assets/img/favicon-16x16.png', revision: '04.08.20.1'},
+  {url: 'assets/img/mhlc.png', revision: '04.08.20.1'},
+  {url: 'assets/img/mhlc-logo.png', revision: '04.08.20.1'},
+  {url: 'assets/img/crosshair.svg', revision: '04.08.20.1'},
+  {url: 'assets/img/ios-share.png', revision: '04.08.20.1'},
+  {url: 'assets/vendor/framework7-5.2.0/css/framework7.bundle.min.css', revision: '04.08.20.1'},
+  {url: 'assets/vendor/openlayers-6.1.1/ol.css', revision: '04.08.20.1'},
+  {url: 'assets/css/app.css', revision: '04.08.20.1'},
+  {url: 'assets/fonts/MaterialIcons-Regular.woff2', revision: '04.08.20.1'},
+  {url: 'assets/vendor/framework7-5.2.0/js/framework7.bundle.min.js', revision: '04.08.20.1'},
+  {url: 'assets/vendor/openlayers-6.1.1/ol.js', revision: '04.08.20.1'},
+  {url: 'assets/vendor/proj4js-2.5.0/proj4.js', revision: '04.08.20.1'},
+  {url: 'assets/vendor/kompas-0.0.1/kompas.js', revision: '04.08.20.1'},
+  {url: 'assets/js/app.js', revision: '04.08.20.1'},
+  {url: 'maps/2018-Wolf-Creek-Falls-Preserve-Map.jpg', revision: '04.08.20.1'},
+  {url: 'maps/AlbCo_RailTrail.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Bennett-Hill-Preserve-2017-new-logo.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Bozenkill_Preserve2.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Fox_Preserve-10-3-17.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Holt_Preserve_2019.jpg', revision: '04.08.20.2'},
+  {url: 'maps/MHLC-Keleher-Preserve-Update-2017.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Mosher-Marsh-Update-2017.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Normanskill_Preserve.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Phillipinkill_Preserve.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Schiffendecker_Preserve.jpg', revision: '04.08.20.1'},
+  {url: 'maps/SchoharieCreek_Preserve.jpg', revision: '04.08.20.1'},
+  {url: 'maps/StrawberryFields_Preserve.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Swift_Preserve1.jpg', revision: '04.08.20.1'},
+  {url: 'maps/VanDyke_Entire_Preserve.jpg', revision: '04.08.20.1'},
+  {url: 'maps/Winn_Preserve-2017-map.jpg', revision: '04.08.20.1'}
+]);
+
 self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
+    caches.has('precache-12.15.19.2').then(function(hasCache) {
+      if (hasCache) {
+        caches.delete('precache-12.15.19.2');
+      }
+    })
   );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        /* Disable runtime caching
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-        */
-
-        return fetch(event.request).then(response => {
-          return response;
-        });
-      })
-    );
-  }
 });
