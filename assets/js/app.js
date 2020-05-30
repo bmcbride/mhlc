25545e61bc8b7aa6f6c054a8c9b014249a31d824
Bryan McBride <mcbride.bryan@gmail.com>
2020-05-29 22:16:04 -0400

    update NYSDOP WMS URL

--- assets/js/app.js
+++ assets/js/app.js
@@ -111,7 +111,7 @@ app.layers = {
     }),
     nysdop: new ol.layer.Tile({
       source: new ol.source.TileWMS({
-        url: "https://orthos.dhses.ny.gov/ArcGIS/services/Latest/MapServer/WMSServer",
+        url: "https://orthos.its.ny.gov/arcgis/services/wms/Latest/MapServer/WmsServer",
         attributions: "<a href='http://orthos.dhses.ny.gov/arcgis/rest/' class='external'>NYS</a>",
         params: {
           "LAYERS": "0,1,2,3,4",
