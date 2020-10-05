0e0dc76a3cd2eb19695a1d0969bd154802610512
Bryan McBride <mcbride.bryan@gmail.com>
2020-10-05 09:18:18 -0400

    Update Touhey link and description

--- service-worker.js
+++ service-worker.js
@@ -6,9 +6,9 @@ workbox.setConfig({
 });
 
 workbox.precaching.precacheAndRoute([
-  {url: 'index.html', revision: '09.30.20.3'},
-  {url: './', revision: '09.30.20.3'}, // Alias for index.html
-  {url: 'maps.json', revision: '09.29.20.1'},
+  {url: 'index.html', revision: '10.05.20.1'},
+  {url: './', revision: '10.05.20.1'}, // Alias for index.html
+  {url: 'maps.json', revision: '10.05.20.1'},
   {url: 'assets/img/apple-touch-icon.png', revision: '04.08.20.1'},
   {url: 'assets/img/android-chrome-192x192.png', revision: '04.08.20.1'},
   {url: 'assets/img/favicon-32x32.png', revision: '04.08.20.1'},
