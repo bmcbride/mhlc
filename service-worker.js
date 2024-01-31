c1720931b0998a8d729213aa6f0eb946dae40b91
Bryan McBride <mcbride.bryan@gmail.com>
2024-01-31 17:09:33 -0500

    Update Mosher Marsh trail map

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ workbox.setConfig({
 });
 
 workbox.precaching.precacheAndRoute([
-  {url: 'index.html', revision: '01.18.24.1'},
-  {url: 'maps.json', revision: '01.18.24.1'},
+  {url: 'index.html', revision: '01.31.24.1'},
+  {url: 'maps.json', revision: '01.31.24.1'},
   {url: 'assets/img/apple-touch-icon.png', revision: '04.08.20.1'},
   {url: 'assets/img/android-chrome-192x192.png', revision: '04.08.20.1'},
   {url: 'assets/img/favicon-32x32.png', revision: '04.08.20.1'},
@@ -36,7 +36,7 @@ workbox.precaching.precacheAndRoute([
   {url: 'maps/Holt_Trail_Map_Jan2024.jpg', revision: '01.18.24.1'},
   {url: 'maps/Hollyhock_Hollow_2020.jpg', revision: '05.29.20.1'},
   {url: 'maps/MHLC-Keleher-Preserve-Update-2017.jpg', revision: '04.08.20.1'},
-  {url: 'maps/Mosher-Marsh-Update-2017.jpg', revision: '04.08.20.1'},
+  {url: 'maps/MosherTrails_2023.jpg', revision: '01.31.24.1'},
   {url: 'maps/Normanskill_Preserve.jpg', revision: '04.08.20.1'},
   {url: 'maps/Phillipinkill_Preserve.jpg', revision: '04.08.20.1'},
   {url: 'maps/SchiffendeckerFarm_2022.jpg', revision: '10.21.22.1'},
