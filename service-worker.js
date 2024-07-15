3eb860b90cab50ef8437ba575fdd86ba1e43821d
Bryan McBride <mcbride.bryan@gmail.com>
2024-07-15 15:46:40 -0400

    update MBFP & MM maps

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ workbox.setConfig({
 });
 
 workbox.precaching.precacheAndRoute([
-  {url: 'index.html', revision: '01.31.24.1'},
-  {url: 'maps.json', revision: '01.31.24.1'},
+  {url: 'index.html', revision: '07.15.24.1'},
+  {url: 'maps.json', revision: '07.15.24.1'},
   {url: 'assets/img/apple-touch-icon.png', revision: '04.08.20.1'},
   {url: 'assets/img/android-chrome-192x192.png', revision: '04.08.20.1'},
   {url: 'assets/img/favicon-32x32.png', revision: '04.08.20.1'},
@@ -29,14 +29,14 @@ workbox.precaching.precacheAndRoute([
   {url: 'assets/js/app.js', revision: '01.04.22.2'},
   {url: 'maps/AlbCo_RailTrail.jpg', revision: '04.08.20.1'},
   {url: 'maps/AshfordGlen_2023.jpg', revision: '07.07.22.1'},
-  {url: 'maps/BenderMelonFarmPreserve_2022.jpg', revision: '12.21.22.1'},
+  {url: 'maps/BMFP_2024.jpg', revision: '07.15.24.1'},
   {url: 'maps/Bennett_Hill_Map_2019.jpg', revision: '04.10.20.1'},
   {url: 'maps/Bozenkill_Preserve2.jpg', revision: '04.08.20.1'},
   {url: 'maps/Fox_Preserve-10-3-17.jpg', revision: '04.08.20.1'},
   {url: 'maps/Holt_Trail_Map_Jan2024.jpg', revision: '01.18.24.1'},
   {url: 'maps/Hollyhock_Hollow_2020.jpg', revision: '05.29.20.1'},
   {url: 'maps/MHLC-Keleher-Preserve-Update-2017.jpg', revision: '04.08.20.1'},
-  {url: 'maps/MosherTrails_2023.jpg', revision: '01.31.24.1'},
+  {url: 'maps/Mosher_Marsh_2024.jpg', revision: '07.15.24.1'},
   {url: 'maps/Normanskill_Preserve.jpg', revision: '04.08.20.1'},
   {url: 'maps/Phillipinkill_Preserve.jpg', revision: '04.08.20.1'},
   {url: 'maps/SchiffendeckerFarm_2022.jpg', revision: '10.21.22.1'},
